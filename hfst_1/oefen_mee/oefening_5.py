8cd6dac60188b6400a902244aeb5d51ebd407249
Vami220805 <vami220805@leerling.mosa-rt.be>
2022-09-19 11:48:23 +0200

    update

--- hfst_1/oefen_mee/oefening_5.py
+++ hfst_1/oefen_mee/oefening_5.py
@@ -5,6 +5,7 @@ Voorbeeld:
 >>> Dict    = {"Dit": "tiD", "is": "si", "een": "nee", "zin": "niz"}
 
 Tip: je hebt al in de reeksen gezien hoe een woord om te keren.
+
 """
 
 zin = input("Geef een zin op: ")
