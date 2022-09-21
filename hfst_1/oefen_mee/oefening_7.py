2a93f527c8b5140d1d1b01dbb266a919e4ec79c0
Vami220805 <vami220805@leerling.mosa-rt.be>
2022-09-21 09:17:28 +0200

    update

--- hfst_1/oefen_mee/oefening_7.py
+++ hfst_1/oefen_mee/oefening_7.py
@@ -3,15 +3,30 @@ lijst_lijst = [
     [5, 6, 7, 8],
     [9, 10]
 ]
+# for lijst in lijst_lijst:
+#     for element in lijst:
+#         print(element)
+
 
 lijst_dict = [
     {'foo':12, 'bar':14 },
     {'moo':52, 'car':641},
     {'doo':6 , 'tar':84, 'var':38 }
 ]
+# for dict in lijst_dict:
+#     for key in dict:
+#         print(key)
+#         print(dict[key])
+
 
 dict_dict = {
     1: {'naam': 'John',  'leeftijd': 27, 'geslacht': 'Man'},
     2: {'naam': 'Marie', 'leeftijd': 22, 'geslacht': 'Vrouw'},
     3: {'naam': 'Kurt',  'leeftijd': 23}
 }
+
+for key in dict_dict:  
+    print(key)
+    for element in dict_dict[key]:
+        print(element)
+        print(dict_dict[key][element])
