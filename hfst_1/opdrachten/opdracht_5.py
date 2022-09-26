675d6879955533104e3a798193124780f9ad4b71
Vami220805 <vami220805@leerling.mosa-rt.be>
2022-09-26 11:39:38 +0200

    update

--- hfst_1/opdrachten/opdracht_5.py
+++ hfst_1/opdrachten/opdracht_5.py
@@ -1,6 +1,3 @@
-from unittest.mock import NonCallableMagicMock
-
-
 poll_talen = ["Lucas", "Maud", "Jan", "Dillan", 
               "Piet", "Joris"]
 
@@ -12,12 +9,12 @@ favorite_languages = {
 for naam in poll_talen:
     taal = favorite_languages.get(naam)
     if taal == None: 
-        favoriete_taal= input("Neem alstublieft deel aan onze poll ")
+        favoriete_taal= input(f"{naam}, neem alstublieft deel aan onze poll ")
         if favoriete_taal== '':
             print("leeg")
         else: 
             favorite_languages[naam] = favoriete_taal
+            print("bedankt!")
     else: 
-        print("Bedankt voor deelnemen!")
-
+        print(f"{naam}, bedankt voor deelnemen!")
 print(favorite_languages)
--- hfst_1/opdrachten/opdracht_6.py
+++ hfst_1/opdrachten/opdracht_6.py
@@ -1,8 +1,23 @@
 # Open lied.txt in Python
-lied = open("lied.txt", "r")
+lied = open("hfst_1/opdrachten/lied_2.txt", "r")
 # Vorm lied om naar lijst, vervang witregels '\n' door spaties ' ' 
 lied = lied.read().replace('\n', ' ') 
 # Test inhoud van lied
 print(lied)
 
 """ Begin eigen code hier """
+
+# niveau 1
+# lijst_lied = lied.split()
+# dict = {}
+# for woord in lijst_lied:
+#     if woord not in dict:
+#         dict[woord] = lijst_lied.count(woord)
+# print(dict)
+
+lijst_lied = lied.split()
+dict = {}
+for woord in lijst_lied:
+    if woord not in dict:
+        dict[woord] = lijst_lied.count(woord)
+print(dict)
