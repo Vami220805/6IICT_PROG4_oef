0f6c1708d26df1d6db967e485eca965ff68be35e
Vami220805 <vami220805@leerling.mosa-rt.be>
2022-09-26 10:50:17 +0200

    update

--- hfst_1/opdrachten/opdracht_4.py
+++ hfst_1/opdrachten/opdracht_4.py
@@ -12,7 +12,20 @@ dict = {
     }
 }
 
+# print(dict["belgie"]["provincie"]["weetjes"]["gouverneur"])
+
 """ Niveau 2 """
+
+dict["belgie"]["provincie"]["informatie"] = dict["belgie"]["provincie"].pop("weetjes")
+
+# print(dict)
+
+
+""" Niveau 3"""
 extra_info = [  ["mannen", 49.77], 
                 ["vrouwen", 50.23], 
                 ["hoofdstad", "hasselt"] ]
+
+for lijst in extra_info:
+    dict["belgie"]["provincie"]["informatie"][lijst[0]]= lijst[1]
+print(dict)
--- hfst_1/opdrachten/opdracht_5.py
+++ hfst_1/opdrachten/opdracht_5.py
@@ -1,3 +1,6 @@
+from unittest.mock import NonCallableMagicMock
+
+
 poll_talen = ["Lucas", "Maud", "Jan", "Dillan", 
               "Piet", "Joris"]
 
@@ -5,3 +8,16 @@ favorite_languages = {
     "Jan": "python",    
     "Piet": "c",    
     "Joris": "ruby"}
+
+for naam in poll_talen:
+    taal = favorite_languages.get(naam)
+    if taal == None: 
+        favoriete_taal= input("Neem alstublieft deel aan onze poll ")
+        if favoriete_taal== '':
+            print("leeg")
+        else: 
+            favorite_languages[naam] = favoriete_taal
+    else: 
+        print("Bedankt voor deelnemen!")
+
+print(favorite_languages)
