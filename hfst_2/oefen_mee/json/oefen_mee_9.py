29dcfa82ff15dc34db88e73701abd5e374f47bfd
Vami220805 <90028381+Vami220805@users.noreply.github.com>
2022-10-17 11:57:02 +0200

    update

--- hfst_2/oefen_mee/json/oefen_mee_9.py
+++ hfst_2/oefen_mee/json/oefen_mee_9.py
@@ -2,7 +2,7 @@ import json
 
 # Pad is afhankelijk van locatie appel.json
 fp = open("hfst_2/oefen_mee/json/oefen_mee_9.json", "r")
-agenda = json.load(fp)
+bestand = json.load(fp)
 fp.close()  # Na sluiten is JSON niet meer bruikbaar
 
-print(agenda)
+print(bestand)
