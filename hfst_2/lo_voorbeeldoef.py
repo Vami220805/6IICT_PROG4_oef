d2c0e3fd3535e3345b1b5df8ca89880242742229
Vami220805 <90028381+Vami220805@users.noreply.github.com>
2022-11-30 10:08:52 +0100

    update

--- hfst_2/lo_voorbeeldoef.py
+++ hfst_2/lo_voorbeeldoef.py
@@ -3,6 +3,7 @@ Schrijf bij iedere regel code commentaar
 De code mag geen foutmeldingen geven
     Opgelet! Code in commentaar wordt niet bekeken.
 """
+import json, random
 
 """ 
 lo_voorbeeldoefening.json bevat info over het schaakstuk "toren".
@@ -14,6 +15,9 @@ laad lo_voorbeeldoefening.json in Python. Zet deze dictionary in een variabele.
 Lukt dit niet? Dan mag je de dictionary rechtstreeks hieronder plakken.
                Je krijgt dan wel geen punten voor dit onderdeel.
 """
+fp = open("hfst_2/lo_voorbeeldoefening.json", "r")
+dic = json.load(fp)
+fp.close()  # Na sluiten is JSON niet meer bruikbaar
 
 """ STAP 2:
 print volgende zaken over de toren:
@@ -24,6 +28,11 @@ print volgende zaken over de toren:
 Je moet deze info uit de dictionary halen (dus niet manueel invullen).
 """
 
+for key in dic:
+    print(key)
+print(dic["toren"]["waarde_pionnen"])
+print(dic["toren"]["beweging"][random.randint(0,3)])
+
 """ STAP 3:
 Voeg volgende zaken toe aan de dictionary (links staat de key, rechts de value):
     - engelse_naam: rook
@@ -31,7 +40,13 @@ Voeg volgende zaken toe aan de dictionary (links staat de key, rechts de value):
 
 """
 
+dic["toren"]["engelse_naam"] = "rook"
+dic["toren"]["andere_naam"]= ["Toring", "Torra", "Rukhkh", "Top"]
+
 """ STAP 4:
 Schrijf de gewijzigde dictionary weg naar een NIEUW JSON-bestand.
 Bijvoorbeeld lo_voorbeeldresultaat.json .
 """
+fp1 = open("hfst_2/lo_voorbeeldresultaat.json", "w")
+json.dump(dic, fp1)
+fp.close() # Na sluiten is JSON niet meer bruikbaar
