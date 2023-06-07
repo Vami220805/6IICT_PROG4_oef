f54ef1f584d7ccee71b505892a790da2f255181a
Vami220805 <90028381+Vami220805@users.noreply.github.com>
2023-06-07 08:36:09 +0200

    update

--- hfst_8_recursie/oefeningen/oefenmee4.py
+++ hfst_8_recursie/oefeningen/oefenmee4.py
@@ -1,11 +1,30 @@
 """ Niveau 1: los sommatie recursief op """
 
-print( sommatie(1) )   # 1
-print( sommatie(2) )   # 3
-print( sommatie(3) )   # 6
-print( sommatie(4) )   # 10
-print( sommatie(5) )   # 15
-print( sommatie(100) ) # 5050
+def sommatie(n):   
+    if n == 1:
+        return n
+    vorige = sommatie(n-1)
+    return n + vorige
+
+
+# print( sommatie(1) )   # 1
+# print( sommatie(2) )   # 3
+# print( sommatie(3) )   # 6
+# print( sommatie(4) )   # 10
+# print( sommatie(5) )   # 15
+# print( sommatie(100) ) # 5050
 
 
 """ Niveau 2: los sommatie met while-loops op """
+
+def sommatie_while(n):
+    uitkomst = 0
+    while True:
+        if n < 1:
+            return uitkomst
+        uitkomst +=n
+        n -= 1
+
+
+print( sommatie_while(4) )   # 10
+
